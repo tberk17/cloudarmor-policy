ad9eee9556f1988412c6abf05863852d59133cae
tberk17 <turanberk.arslan@metro.de>
2026-01-28 13:30:52 +0100

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,15 @@
+
+= "GCP project ID where Infra Manager will deploy"
+  type        = string
+}
+
+variable "region" {
+  description = "Default region for provider (not a resource location)"
+  type        = string
+  default     = "europe-west1"
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
