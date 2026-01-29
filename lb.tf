3d5185e5d1f14e9ab1f746e1084c175c946f5a83
tberk17 <turanberk.arslan@metro.de>
2026-01-29 13:43:32 +0100

    Update lb.tf

--- lb.tf
+++ lb.tf
@@ -1,28 +1,16 @@
-# lb.tf
-
-# 1) Unmanaged instance group containing the existing VM
-resource "google_compute_instance_group" "unmanaged" {
-  name = "${var.lb_name_prefix}-igm"
+# Use your existing unmanaged instance group
+data "google_compute_instance_group" "igm" {
+  name = var.instance_group_name
   zone = var.zone
-
-  # Reference existing VM by self_link
-  instances = [
-    "https://www.googleapis.com/compute/v1/projects/${var.project_id}/zones/${var.zone}/instances/${var.vm_name}"
-  ]
-
-  named_port {
-    name = "http"
-    port = var.service_port
-  }
 }
 
-# 2) Health check (HTTP)
+# Health check (HTTP) – use explicit port
 resource "google_compute_health_check" "http" {
   name = "${var.lb_name_prefix}-hc"
 
   http_health_check {
-    port_specification = "USE_NAMED_PORT"
-    request_path       = var.health_check_path
+    port         = var.service_port
+    request_path = var.health_check_path
   }
 
   check_interval_sec  = 5
@@ -31,45 +19,69 @@ resource "google_compute_health_check" "http" {
   unhealthy_threshold = 2
 }
 
-# 3) Backend service with Cloud Armor attached
+# Backend service with Cloud Armor attached
 resource "google_compute_backend_service" "app_backend" {
   name        = "${var.lb_name_prefix}-bes"
   protocol    = "HTTP"
-  port_name   = "http"
+  port_name   = "http"    # requires a named port "http" on the instance group
   timeout_sec = 30
 
   health_checks = [google_compute_health_check.http.id]
 
   backend {
-    group = google_compute_instance_group.unmanaged.self_link
+    group = data.google_compute_instance_group.igm.self_link
   }
 
-  # <-- Attach the Cloud Armor policy created by your module
-  security_policy = data.google_compute_security_policy.baseline.self_link
+  # Attach Cloud Armor policy (enforcement point)
+  security_policy = data.google_compute_security_policy.policy.self_link
 }
 
-# 4) URL map
+# URL map
 resource "google_compute_url_map" "default" {
   name            = "${var.lb_name_prefix}-urlmap"
   default_service = google_compute_backend_service.app_backend.self_link
 }
 
-# 5) HTTP proxy
+# HTTP proxy
 resource "google_compute_target_http_proxy" "default" {
   name    = "${var.lb_name_prefix}-http-proxy"
   url_map = google_compute_url_map.default.self_link
 }
 
-# Optional: reserve a static global IP so you can point DNS at it
+# Global IP
 resource "google_compute_global_address" "lb_ip" {
   name = "${var.lb_name_prefix}-ip"
 }
 
-# 6) Global forwarding rule (HTTP, port 80)
+# HTTP forwarding rule (80)
 resource "google_compute_global_forwarding_rule" "http" {
   name                  = "${var.lb_name_prefix}-http-fr"
   target                = google_compute_target_http_proxy.default.self_link
   port_range            = "80"
+  load_balancing_scheme = "EXTERNAL"
+  ip_protocol           = "TCP"
+  ip_address            = google_compute_global_address.lb_ip.address
+}
+
+# === Optional HTTPS (if var.domain set) ===
+resource "google_compute_managed_ssl_certificate" "cert" {
+  count = var.domain != "" ? 1 : 0
+  name  = "${var.lb_name_prefix}-cert"
+  managed { domains = [var.domain] }
+}
+
+resource "google_compute_target_https_proxy" "https" {
+  count            = var.domain != "" ? 1 : 0
+  name             = "${var.lb_name_prefix}-https-proxy"
+  url_map          = google_compute_url_map.default.self_link
+  ssl_certificates = [google_compute_managed_ssl_certificate.cert[0].self_link]
+}
+
+resource "google_compute_global_forwarding_rule" "https" {
+  count                 = var.domain != "" ? 1 : 0
+  name                  = "${var.lb_name_prefix}-https-fr"
+  target                = google_compute_target_https_proxy.https[0].self_link
+  port_range            = "443"
   load_balancing_scheme = "EXTERNAL"
   ip_protocol           = "TCP"
   ip_address            = google_compute_global_address.lb_ip.address
