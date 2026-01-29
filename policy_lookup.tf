e349008ee2e12e49490ef43891dcdadfb5818fa4
tberk17 <turanberk.arslan@metro.de>
2026-01-29 14:01:02 +0100

    Update policy_lookup.tf

--- policy_lookup.tf
+++ policy_lookup.tf
@@ -1,5 +1,4 @@
-# Get the policy that the module just created (by name)
 data "google_compute_security_policy" "policy" {
   name    = local.policy_name
-  project = var.project_id
+  project = var.projectId
 }
