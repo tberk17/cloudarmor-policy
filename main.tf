8351ac25f852fb9b0b5a1688c29c5e2410a91b73
tberk17 <turanberk.arslan@metro.de>
2026-01-29 10:42:28 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
 # Create a Cloud Armor implementing METROs baseline.
 module "cloudarmor_policy" {
   source     = "metro-digital/cf-cloudarmor-policy/google"
